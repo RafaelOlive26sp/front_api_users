0291799e335643276c78c577c3c7998fbd1a03e7
Rafael Delgado <oliveirarafael22@outlook.com.br>
2024-12-22 10:58:21 -0300

    feat: Rotas.
    Adicionado as rotas iniciais do projeto junto com sua verificação de autorização

--- src/router/index.js
+++ src/router/index.js
@@ -1,23 +1,38 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import HomeLogin from '../views/LoginView.vue'
+import DashboardView from '@/views/DashboardView.vue'
+
+import store from '@/store/index.js'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: '/',
-      name: 'home',
-      component: HomeView,
+      name: 'login',
+      component: HomeLogin,
+      meta:{guest:true},
     },
     {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue'),
+      path: '/dashboard',
+      name: 'dashboard',
+      component: DashboardView,
+      meta:{requiresAuth:true},
     },
+
   ],
+})
+
+router.beforeEach((to, from, next) => {
+  const isAuthenticated = store.state.auth.token;
+
+  if (to.meta.requiresAuth && !isAuthenticated) {
+    next({name: 'login'})
+  }else if (to.meta.guest && isAuthenticated) {
+    next({name: 'dashboard'})
+  }else {
+    next()
+  }
 })
 
 export default router
