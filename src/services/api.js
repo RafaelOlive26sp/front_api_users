c914b6b0e86c5c9b21fee64ef7171058727ba7a3
Rafael Delgado <oliveirarafael22@outlook.com.br>
2024-12-22 11:56:12 -0300

    feat(URL): adicionado v1 na url

--- src/services/api.js
+++ src/services/api.js
@@ -3,10 +3,10 @@ import Cookies from "js-cookie";
 
 
 const api = axios.create({
-  baseURL: "http://127.0.0.1:8000/api",
+  baseURL: "http://127.0.0.1:8000/api/v1/",
   headers: {
     "Content-Type": "application/json",
-    "Accept": "application/json",
+    Accept: "application/json",
   }
 })
  api.interceptors.request.use((config) => {
