18ac0e1e342b58341f2b7b95122788a88b6b6120
Rafael Delgado <oliveirarafael22@outlook.com.br>
2024-12-22 10:53:43 -0300

    feat: Autenticação do Usuario
    Agrupara toda a logica para a autenticação do usuario

---
+++ src/store/modules/auth.js
@@ -0,0 +1,13 @@
+import api from '@/services/api.js'
+import router from '@/router/index.js'
+import Cookies from 'js-cookie'
+
+const authModules = {
+  namespaced: true,
+  state: {},
+  mutations: {},
+  actions: {},
+  getters: {},
+
+}
+export default authModules
