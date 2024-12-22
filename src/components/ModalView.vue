e9be57c3dbbd4088ee3e74add167cb09b2661cee
Rafael Delgado <oliveirarafael22@outlook.com.br>
2024-12-22 12:41:16 -0300

    fetch: Modal
    ModalView.vue

---
+++ src/components/ModalView.vue
@@ -0,0 +1,28 @@
+
+<template>
+  <div class="modal fade" :id=idModal tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+    <div class="modal-dialog">
+      <div class="modal-content text-dark">
+        <slot name="content"></slot>
+      </div>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  name: 'ModalView',
+  components: {
+
+  },
+  props: {
+    idModal: {
+      type: String,
+      default: 'modalChoice',
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
