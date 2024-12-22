e333f563d3b4ccd754a0f6ff2aa9a5d7bf1c184e
Rafael Delgado <oliveirarafael22@outlook.com.br>
2024-12-22 12:38:46 -0300

    feat(actions, Mutations): Criado os metodos de autenticação.
    Criado o metodo Login e o Metodo Logout

--- src/store/modules/auth.js
+++ src/store/modules/auth.js
@@ -4,9 +4,38 @@ import Cookies from 'js-cookie'
 
 const authModules = {
   namespaced: true,
-  state: {},
-  mutations: {},
-  actions: {},
+  state: {
+    token: Cookies.get('access_token') || null,
+  },
+  mutations: {
+    SET_AUTH_TOKEN(state, token) {
+      state.token = token
+      Cookies.set('access_token', token)
+    },
+    LOGOUT(state) {
+      state.AUTH_TOKEN = null
+        Cookies.remove('access_token')
+    }
+  },
+  actions: {
+    async login({ commit }, credentials) {
+      try{
+       // const response = await api.post('/login', credentials ,{headers:{'X-Origin':'dashboard'}});
+       const response = await api.post('/login', credentials);
+        console.log(response.data)
+        commit('SET_AUTH_TOKEN', response.data.access_token);
+        await router.push({ name: 'dashboard' });
+
+      }catch(error){
+        console.log(error)
+
+      }
+    },
+    async logout({ commit }) {
+      commit('LOGOUT');
+      router.push({ name: 'dashboard' });
+    }
+  },
   getters: {},
 
 }
