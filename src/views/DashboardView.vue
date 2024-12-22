a472ea5d0cc8853784cf73a1af9e0be6934b567b
Rafael Delgado <oliveirarafael22@outlook.com.br>
2024-12-22 15:04:11 -0300

    feat: template de navbartop

--- src/views/DashboardView.vue
+++ src/views/DashboardView.vue
@@ -1,21 +1,27 @@
 
 <template>
   <div class="text-light">
-    <nav-bar-top-view></nav-bar-top-view>
-    <side-bar-view></side-bar-view>
+    <nav-bar-top-view>
+      <template v-slot:content>
+          <h2>ola</h2>
+      </template>
+    </nav-bar-top-view>
+
+
+
 
   </div>
 </template>
 <script>
 
-import SideBarView from '@/components/menuNavBars/sideBarView.vue'
+
 import NavBarTopView from '@/components/menuNavBars/navBarTopView.vue'
 
 export default {
   name: 'DashboardView',
   components: {
     NavBarTopView,
-    SideBarView
+
 
   },
   data() {
