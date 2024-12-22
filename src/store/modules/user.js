09c1b7fee48a38f7c6d5a6aa3a2382f9854dc314
Rafael Delgado <oliveirarafael22@outlook.com.br>
2024-12-22 10:54:22 -0300

    feat: Dados do usuario
    Agrupara toda a logica relacionada ao Usuario

---
+++ src/store/modules/user.js
@@ -0,0 +1,12 @@
+import api from '@/services/api.js'
+import Cookies from 'js-cookie'
+
+const userModules = {
+  namespaced: true,
+  state: {},
+  mutations: {},
+  actions: {},
+  getters: {},
+
+}
+export default userModules
