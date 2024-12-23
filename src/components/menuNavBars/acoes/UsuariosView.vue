5f097ac136e3c6733632d48bc63b6c9b5d0926d4
Rafael <oliveirarafael22@outlook.com.br>
2024-12-23 02:56:58 -0300

    feat: enhance UsuariosView layout with Bootstrap for improved styling and structure

--- src/components/menuNavBars/acoes/UsuariosView.vue
+++ src/components/menuNavBars/acoes/UsuariosView.vue
@@ -1,6 +1,16 @@
 <template lang="">
-  <div>
-    <h1>Usuarios</h1>
+  <div class="container ">
+    <div class="row justify-content-center">
+      <div class="col-md-3 border border-primary ">
+        <h5>Numeros de Usuarios</h5>
+      </div>
+      <div class="col-md-3 border border-primary mx-1">
+        <h5>Usuarios Novos</h5>
+      </div>
+      <div class="col-md-3 border border-primary">
+        <h5>Usuarios mais Antigos</h5>
+      </div>
+    </div>
   </div>
 </template>
 <script>
