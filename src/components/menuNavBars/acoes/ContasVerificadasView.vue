9bb90a360fa69a48115bdcf83c62f233a5998135
Rafael <oliveirarafael22@outlook.com.br>
2024-12-23 02:56:33 -0300

    feat: update ContasVerificadasView layout with Bootstrap for improved styling and structure

--- src/components/menuNavBars/acoes/ContasVerificadasView.vue
+++ src/components/menuNavBars/acoes/ContasVerificadasView.vue
@@ -1,5 +1,20 @@
-<template>
-  <div>
-    <h1>Contas Verificadas</h1>
+<template lang="">
+  <div class="container ">
+    <div class="row justify-content-center ">
+      <div class="col-md-3 border border-primary mx-1">
+        <h5>Contas Verificadas</h5>
+      </div>
+      <div class="col-md-3 border border-primary ">
+        <h5>Contas Nao Verificadas</h5>
+      </div>
+    </div>
   </div>
 </template>
+<script>
+export default {
+
+}
+</script>
+<style lang="">
+
+</style>
