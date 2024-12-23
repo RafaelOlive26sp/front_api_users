656f3eff283ef8267231da25c76273c69ced8e59
Rafael <oliveirarafael22@outlook.com.br>
2024-12-23 01:43:11 -0300

    feat: integrate CollapesView component in DashboardView for dynamic content display

--- src/views/DashboardView.vue
+++ src/views/DashboardView.vue
@@ -3,7 +3,13 @@
   <div class="text-light">
     <nav-bar-top-view>
       <template v-slot:content>
-          <h2>ola</h2>
+        <div class="text-dark">
+          <CollapesView :id="$store.state.user.activeCollapse">
+            <template v-slot:content>
+              <component :is="getComponent($store.state.user.activeCollapse)" />
+            </template>
+          </CollapesView>
+        </div>
       </template>
     </nav-bar-top-view>
 
@@ -16,20 +22,47 @@
 
 
 import NavBarTopView from '@/components/menuNavBars/navBarTopView.vue'
+import CollapesView from '@/components/CollapesView.vue'
+// ---------------import components-------------------
+import UsuariosView from '@/components/menuNavBars/acoes/UsuariosView.vue'
+import AtendentesView from '@/components/menuNavBars/acoes/AtendentesView.vue'
+import ContasVerificadasView from '@/components/menuNavBars/acoes/ContasVerificadasView.vue'
+import LogsApi from '@/components/menuNavBars/acoes/LogsApi.vue'
 
 export default {
   name: 'DashboardView',
   components: {
     NavBarTopView,
+    CollapesView,
+    UsuariosView,
+    AtendentesView,
+    ContasVerificadasView,
+    LogsApi,
 
 
   },
   data() {
     return {
-
+      // activeCollapse: null,
+      menuItens:[
+        {id:"collpaseUsers", component:"UsuariosView"},
+        {id:"collpaseAttendant", component:"AtendenteView"},
+        {id:"collpaseAccountVerified", component:"ContasVerificadasView"},
+        {id:"collpaseLogs", component:"LogsApi"},
+      ]
     }
   },
   methods: {
+
+      getComponent(id) {
+        const components = {
+          collapseUsers: "UsuariosView",
+          collapseAttendant: "AtendentesView",
+          collapseAccountVerified: "ContasVerificadasView",
+          collapseLogs: "LogsApi",
+        };
+      return components[id] || null;
+    },
 
   },
   computed:{}
