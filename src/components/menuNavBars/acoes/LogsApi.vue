ad18f5455a40d8dffb9570626943d87b3dc6ca0d
Rafael <oliveirarafael22@outlook.com.br>
2024-12-23 02:56:45 -0300

    feat: update LogsApi component layout with Bootstrap for improved styling and structure

--- src/components/menuNavBars/acoes/LogsApi.vue
+++ src/components/menuNavBars/acoes/LogsApi.vue
@@ -1,5 +1,17 @@
-<template>
-  <div>
-    <h1>Logs</h1>
+<template lang="">
+  <div class="container ">
+    <div class="row justify-content-center text-center">
+      <div class="col-md-3 border border-primary ">
+        <h5>Logs da Api</h5>
+      </div>
+    </div>
   </div>
 </template>
+<script>
+export default {
+
+}
+</script>
+<style lang="">
+
+</style>
