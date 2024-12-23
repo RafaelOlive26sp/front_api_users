a786db8e398c3273816a7298373590699d411e44
Rafael <oliveirarafael22@outlook.com.br>
2024-12-23 01:39:16 -0300

    feat: add CollapesView component for collapsible content

---
+++ src/components/CollapesView.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="collapse" :id="collapseUsers">
+    <div class="card card-body" >
+      <slot name="content"></slot>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  name: 'CollapesView',
+  props: {
+    collapseUsers: {
+      type: String,
+      default: 'collapseChoise'
+    }
+  }
+
+}
+</script>
+<style lang="">
+
+</style>
