a08148eca7d5430821fba34daf0d67adaa540f90
Rafael <oliveirarafael22@outlook.com.br>
2024-12-27 04:29:44 -0300

    feat: implement user statistics display and fetch functionality in UsuariosView

--- src/components/menuNavBars/acoes/UsuariosView.vue
+++ src/components/menuNavBars/acoes/UsuariosView.vue
@@ -2,19 +2,44 @@
   <div class="container ">
     <div class="row justify-content-center">
       <div class="col-md-3 border border-primary ">
-        <h5>Numeros de Usuarios</h5>
+        <h5>{{$store.state.user.StatisticData.totalAccounts}}</h5>
       </div>
       <div class="col-md-3 border border-primary mx-1">
-        <h5>Usuarios Novos</h5>
+        <h5>{{$store.state.user.StatisticData.newstUser}}</h5>
       </div>
       <div class="col-md-3 border border-primary">
-        <h5>Usuarios mais Antigos</h5>
+        <h5>{{$store.state.user.StatisticData.oldestUser}}</h5>
       </div>
     </div>
   </div>
 </template>
 <script>
+import { mapActions } from 'vuex';
+
 export default {
+  created() {
+    this.fetchdatas();
+  },
+  components:{
+
+  },
+  data() {
+    return {
+
+    }
+  },
+  methods: {
+    ...mapActions('user',['fetchStatisticData']),
+    async fetchdatas(){
+      try {
+       await this.fetchStatisticData();
+
+      } catch (error) {
+        console.error(error);
+
+      }
+    }
+  },
 
 }
 </script>
