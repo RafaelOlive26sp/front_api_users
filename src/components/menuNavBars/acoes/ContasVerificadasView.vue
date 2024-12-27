b143bc9f01ace510643995da2f63a3eca1d68cd0
Rafael <oliveirarafael22@outlook.com.br>
2024-12-27 04:37:13 -0300

    feat: update ContasVerificadasView to display verified and unverified accounts using computed property

--- src/components/menuNavBars/acoes/ContasVerificadasView.vue
+++ src/components/menuNavBars/acoes/ContasVerificadasView.vue
@@ -2,16 +2,32 @@
   <div class="container ">
     <div class="row justify-content-center ">
       <div class="col-md-3 border border-primary mx-1">
-        <h5>Contas Verificadas</h5>
+        <h5>{{dataStatistc.verifiedAccounts}}</h5>
       </div>
       <div class="col-md-3 border border-primary ">
-        <h5>Contas Nao Verificadas</h5>
+        <h5>{{dataStatistc.unverifiedAccounts}}</h5>
       </div>
     </div>
   </div>
 </template>
 <script>
 export default {
+  component:{
+
+  },
+  data() {
+    return {
+
+    }
+  },
+  methods: {
+
+  },
+  computed:{
+    dataStatistc(){
+      return this.$store.state.user.StatisticData;
+    }
+  }
 
 }
 </script>
