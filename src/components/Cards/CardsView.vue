3c81777e67ed6657ee79feb072584ee7cec88851
Rafael Delgado <oliveirarafael22@outlook.com.br>
2024-12-29 20:00:25 -0300

    feat: created.
    created Component recycle

---
+++ src/components/Cards/CardsView.vue
@@ -0,0 +1,34 @@
+
+<template>
+  <div class="col-md-3 ">
+    <div class="card">
+      <div class="card-header">
+        <small>{{tittle}}</small>
+      </div>
+      <div class="card-body">
+        <blockquote class="blockquote mb-0">
+          <slot name="content"></slot>
+
+        </blockquote>
+      </div>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  name: 'CardsView',
+  components: {
+
+  },
+  props: {
+    tittle:{
+      type: String,
+      default: 'Titulo'
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
