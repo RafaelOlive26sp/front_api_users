8e46da1e9b3a8b2f7fba0e447e45e268f3ae01fa
Rafael Delgado <oliveirarafael22@outlook.com.br>
2024-12-30 15:57:26 -0300

    style: Card
    added props

--- src/components/Cards/CardsView.vue
+++ src/components/Cards/CardsView.vue
@@ -1,34 +1,39 @@
-
 <template>
-  <div class="col-md-3 ">
+  <div class="col-md-3">
     <div class="card">
       <div class="card-header">
-        <small>{{tittle}}</small>
+        <small>{{ tittle }}</small>
       </div>
-      <div class="card-body">
+      <div :class="'card-body '+classCustom">
         <blockquote class="blockquote mb-0">
           <slot name="content"></slot>
-
         </blockquote>
+
       </div>
+
     </div>
   </div>
 </template>
 <script>
 export default {
   name: 'CardsView',
-  components: {
-
-  },
+  components: {},
   props: {
-    tittle:{
+    tittle: {
       type: String,
-      default: 'Titulo'
-    }
-  }
+      default: 'Titulo',
+    },
+    classCustom: {
+      type: String,
+      default: 'card-body',
+    },
+  },
 }
 </script>
 
 <style scoped>
-
+.custom-scroll {
+  max-height: 230px;
+  overflow-y: auto;
+}
 </style>
