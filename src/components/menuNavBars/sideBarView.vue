13eea19badf6a4afbb516a7ae29753be5acd9f06
Rafael Delgado <oliveirarafael22@outlook.com.br>
2024-12-30 15:59:07 -0300

    style: options
    added option in sidemenu, Action, with collapse

--- src/components/menuNavBars/sideBarView.vue
+++ src/components/menuNavBars/sideBarView.vue
@@ -22,14 +22,58 @@
         </ul>
 
         <hr />
-        <ul class="nav flex-column mb-auto">
-          <li class="nav-item">
-            <a class="link-light nav-link d-flex align-items-center gap-2" href="#" >
-              <i class="bi bi-people"></i>
-              Current month
+        <div class="nav flex-column mb-auto" >
+
+            <a class=" mx-3 nav-link gap-2 fs-6  d-flex align-items-center link-body-emphasis  p-0" data-bs-toggle="collapse" href="#CollapseAcao" role="button" aria-expanded="false" aria-controls="collapseExample">
+          <i class="bi bi-menu-button"></i>
+              Ações
             </a>
-          </li>
-        </ul>
+
+
+          <CollapesView id="CollapseAcao" classCustom=" collapse card card-body col-11 mx-3 text-body-secondary">
+            <template v-slot:content>
+              <div class="">
+                <ul class="nav flex-column">
+                  <li class="nav-item" data-bs-toggle="collapse" href="#Consultar">
+                    <i class="bi bi-search"></i>
+                    Consultar
+                  </li>
+                    <div class="nav flex-column collapse  mx-3 col-4 text-center rounded-2 p-1" id="Consultar" style="box-shadow: black 0px 0px 4px 0px inset; ">
+                      <small class="border-bottom">Usuarios</small>
+
+                      <small>Atendentes</small>
+                    </div>
+                  <li class="nav-item" data-bs-toggle="collapse" href="#Atualizar">
+                    <i class="bi bi-pencil-square"></i>
+                    Atualizar
+                  </li>
+                  <div class="nav flex-column collapse  mx-3 col-4 text-center rounded-2" id="Atualizar" style="box-shadow: black 0px 0px 4px 0px inset; ">
+                    <small>Contas</small>
+                  </div>
+                  <li class="nav-item" data-bs-toggle="collapse" href="#Deletar">
+                    <i class="bi bi-trash3"></i>
+                    Deletar
+                  </li>
+                  <div class="nav flex-column collapse  mx-3 col-4 text-center rounded-2" id="Deletar" style="box-shadow: black 0px 0px 4px 0px inset; ">
+                    <small>Contas</small>
+                  </div>
+
+                </ul>
+              </div>
+            </template>
+          </CollapesView>
+
+<!--          <div class="collapse " id="CollapseAcao">-->
+<!--            <div class="card card-body col-11 mx-3 text-body-secondary">-->
+<!--              <ul class="nav flex-column mr-auto">-->
+<!--                <li class="nav-item">Acao</li>-->
+<!--                <li class="nav-item">Acao</li>-->
+<!--                <li class="nav-item">Acao</li>-->
+<!--                <li class="nav-item">Acao</li>-->
+<!--              </ul>-->
+<!--            </div>-->
+<!--          </div>-->
+        </div>
 
         <hr class="my-3" />
 
@@ -58,14 +102,17 @@
 </template>
 <script>
 import { mapActions } from 'vuex'
+import CollapesView from '@/components/CollapesView.vue'
+
+
 
 export default {
   name: 'sideBarView',
-  components: {},
+  components: { CollapesView },
   data() {
     return {
       menuItems: [
-        { id: "collapseUsers", label: "Usuários" , class:"bi bi-people" },
+        { id: "collapseUsers", label: "Resumo" , class:"bi bi-people" },
         { id: "collapseAttendant", label: "Atendentes", class:"bi bi-person-badge" },
         { id: "collapseAccountVerified", label: "Contas Verificadas", class:"bi bi-envelope-check" },
         { id: "collapseLogs", label: "Logs", class:"bi bi-file-earmark-bar-graph" },
