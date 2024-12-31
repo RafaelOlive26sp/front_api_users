7afa5bb0fe1692f6055a75e93ead738e752f4071
Rafael <oliveirarafael22@outlook.com.br>
2024-12-31 04:44:20 -0300

    feat: enhance LogsApi component with improved layout and data presentation

--- src/components/Paginas/LogsApi.vue
+++ src/components/Paginas/LogsApi.vue
@@ -1,32 +1,63 @@
 <template>
   <div class="container ">
     <div class="row justify-content-center text-center">
-      <div class="col-md-3 border border-primary ">
-        <h5>Logs da Api</h5>
-        <small><p>Total de Logs: {{ isLogs.totalLogs }}</p></small>
-        <div>
-          <ul v-for="itensLogs in isLogs.logs" :key="itensLogs.id">
-            <li>{{itensLogs.id}}</li>
-            <li>{{itensLogs.user_id}}</li>
-            <li>{{itensLogs.action}}</li>
-            <li>{{itensLogs.endpoint}}</li>
-            <li>{{itensLogs.ip_address}}</li>
-            <li>{{itensLogs.request_data.email}}</li>
-            <li>{{itensLogs.response_data.status}}</li>
-              <hr>
-          </ul>
-        </div>
+      <CardsView tittle="total de logs">
+        <template v-slot:content>
+            <p>{{ isLogs.totalLogs }}</p>
+        </template>
+      </CardsView>
+      <div class="border d-flex dropend">
+        <i class="bi bi-funnel" data-bs-toggle="dropdown" aria-expanded="false"></i>
+        <ul class="dropdown-menu shadow-sm">
+          <li><a class="dropdown-item" href="#">Action</a></li>
+          <li><a class="dropdown-item" href="#">Another action</a></li>
+          <li><a class="dropdown-item" href="#">Something else here</a></li>
+        </ul>
+
+
       </div>
+      <table class="table   border">
+
+        <thead >
+          <tr>
+            <th scope="col">id</th>
+            <th scope="col">user_id</th>
+            <th scope="col">endpoint</th>
+            <th scope="col">action</th>
+            <th scope="col">Ip_address</th>
+            <th scope="col">request_data</th>
+            <th scope="col">response_data</th>
+
+
+
+          </tr>
+        </thead>
+        <tbody>
+          <tr v-for="key in isLogs.logs" :key="key.id">
+            <th scope="row">{{key.id}}</th>
+            <td>{{ key.user_id }}</td>
+            <td>{{ key.endpoint }}</td>
+            <td>{{ key.action }}</td>
+            <td>{{ key.ip_address }}</td>
+            <td>{{ key.request_data.email }}</td>
+            <td>{{ key.response_data.status }}</td>
+          </tr>
+
+        </tbody>
+      </table>
+
     </div>
   </div>
 </template>
 <script>
 import { mapActions, mapGetters } from 'vuex';
+import CardsView from '../Cards/CardsView.vue';
 
 export default {
 
   name:'LogsApi',
   components:{
+    CardsView
 
   },
   data() {
