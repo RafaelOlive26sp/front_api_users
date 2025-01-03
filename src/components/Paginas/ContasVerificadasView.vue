cd493eb1624850e3ec9a9f9ff79603c06aa0b578
Rafael <oliveirarafael22@outlook.com.br>
2025-01-03 11:20:55 -0300

    feat: replace static account display with CardsView and add loading state management

--- src/components/Paginas/ContasVerificadasView.vue
+++ src/components/Paginas/ContasVerificadasView.vue
@@ -1,26 +1,42 @@
 <template >
   <div class="container ">
     <div class="row justify-content-center ">
-      <div class="col-md-3 border border-primary mx-1">
-        <h5>{{dataStatistc.verifiedAccounts}}</h5>
-      </div>
-      <div class="col-md-3 border border-primary ">
+      <CardsView tittle="Total de contas Verificadas">
+        <template v-slot:content>
+          <PlaceHolderLoadingView v-if="isLoading"/>
+          <p v-else>{{ dataStatistc.verifiedAccounts }}</p>
+        </template>
+      </CardsView>
+
+      <!-- <div class="col-md-3 border border-primary ">
         <h5>{{dataStatistc.unverifiedAccounts}}</h5>
-      </div>
+      </div> -->
     </div>
   </div>
 </template>
 <script>
+import CardsView from '@/components/Cards/CardsView.vue'
+import PlaceHolderLoadingView from '@/components/PlaceHolderLoading/PlaceHolderLoadingView.vue'
 export default {
-  component:{
-
+  mounted() {
+    this.LazyLoading();
+  },
+  components: {
+    CardsView,
+    PlaceHolderLoadingView
   },
   data() {
     return {
-
+      isLoading: true
     }
   },
   methods: {
+    LazyLoading() {
+
+      setTimeout(() => {
+        this.isLoading = false;
+      }, 2000);
+    }
 
   },
   computed:{
