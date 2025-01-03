2cd0b64ab748330bc2a0ef360f5eb71d1200b4b5
Rafael <oliveirarafael22@outlook.com.br>
2025-01-03 11:20:43 -0300

    feat: integrate CardProfileView component in AtendentesView for enhanced user display

--- src/components/Paginas/AtendentesView.vue
+++ src/components/Paginas/AtendentesView.vue
@@ -1,14 +1,35 @@
 <template >
   <div class="container ">
     <div class="row justify-content-center text-center">
-      <div class="col-md-3 border border-primary ">
-        <h5>Atendentes</h5>
-      </div>
+
+      <CardProfileView >
+        <template v-slot:header>
+          <div class=" ">
+            <div class=" text-start">
+              <p class=" m-0 fs-5">Nome</p>
+              <p class=" m-0 fw-semibold text-body-secondary ">
+                teste
+              </p>
+            </div>
+            <img class="rounded-circle bg-secondary" src="" alt="" width="150" height="150" >
+          </div>
+
+        </template>
+        <template v-slot:content>
+
+        </template>
+      </CardProfileView>
+
+
     </div>
   </div>
 </template>
 <script>
+import CardProfileView from '@/components/Cards/CardProfileView.vue'
 export default {
+  components: {
+    CardProfileView
+  }
 
 }
 </script>
