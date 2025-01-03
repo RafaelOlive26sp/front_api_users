b2e4f113cc9d5eac3fcfd0121f2667c26094ed61
Rafael <oliveirarafael22@outlook.com.br>
2025-01-03 11:20:19 -0300

    feat: add CardProfileView component for displaying user profile cards

---
+++ src/components/Cards/CardProfileView.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="col-md-3">
+    <div class="card">
+      <div class="card-header">
+        <slot name="header"></slot>
+      </div>
+      <div :class="' '+classCustom">
+        <blockquote class="blockquote mb-0">
+          <slot name="content"></slot>
+        </blockquote>
+
+      </div>
+
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  name: 'CardsView',
+  components: {},
+  props: {
+    tittle: {
+      type: String,
+      default: 'Coloque seu Titulo, com tittle',
+    },
+    classCustom: {
+      type: String,
+      default: '',
+    },
+  },
+}
+</script>
+
+<style scoped>
+.custom-scroll {
+  max-height: 230px;
+  overflow-y: auto;
+}
+</style>
