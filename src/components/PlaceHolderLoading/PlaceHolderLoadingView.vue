c616147634b446cd73199fa1a06e6ac996ab31fa
Rafael <oliveirarafael22@outlook.com.br>
2025-01-03 09:27:53 -0300

    feat: add PlaceHolderLoadingView component for improved loading experience

---
+++ src/components/PlaceHolderLoading/PlaceHolderLoadingView.vue
@@ -0,0 +1,20 @@
+<template>
+  <p class="placeholder-glow">
+  <span :class="ClassCuston"></span>
+</p>
+
+</template>
+
+<script>
+export default {
+  name: 'PlaceHolderLoadingView',
+  props: {
+    ClassCuston: {
+      type: String,
+      default: 'placeholder col-2 rounded-2',
+    },
+  },
+}
+</script>
+
+
