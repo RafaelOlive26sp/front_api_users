1a7e0fc547638f4e15017289a5f668d9ceeac090
Rafael <oliveirarafael22@outlook.com.br>
2025-01-08 04:30:27 -0300

    feat: integrate Vuex state management in AcoesView component; display user data

--- src/components/Paginas/AcoesView.vue
+++ src/components/Paginas/AcoesView.vue
@@ -1,10 +1,30 @@
 
 <template>
   <p>Açoes</p>
+
+  {{ datas }}
+  <pre>
+
+    <!-- {{ $store.state }} -->
+  </pre>
+
 </template>
 <script>
+import { mapState } from 'vuex';
+
 export default {
-  name: 'AcoesView'
+  name: 'AcoesView',
+  data() {
+    return {
+
+    }
+  },
+  methods: {
+
+  },
+  computed:{
+    ...mapState('user',['datas'])
+  }
 }
 </script>
 
