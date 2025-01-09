c83c976bb32e72b0971f81a0fd01b12b8b09f81e
Rafael Delgado <oliveirarafael22@outlook.com.br>
2025-01-09 12:00:43 -0300

    feat: viewAcoes
    was created Card for display datas the user

--- src/components/Paginas/AcoesView.vue
+++ src/components/Paginas/AcoesView.vue
@@ -1,33 +1,61 @@
-
 <template>
   <p>Açoes</p>
+  <CardsView classDivFather="col-md-3" :tittle="'Id: ' + datas?.id">
+    <template v-slot:content>
+      <div class="">
+        <div class="col-12">
+          <div class="card">
+            <div class="card-body d-flex align-items-start">
+              <div class="me-3">
+                <img
+                  src="https://www.w3schools.com/howto/img_avatar.png"
+                  alt="Avatar"
+                  class="avatar rounded-4 "
+                  height="100"
+                  width="100"
+                  style="
+                    box-shadow: 2px 2px 5px -2px black;"
+                />
+              </div>
+              <div class="mt-3">
+                <small>Name:
+                  {{ datas?.name }}</small>
+                <br>
+                <small>Email:
+                  {{ datas?.email }}</small>
+<!--                <p>Created At: {{ datas?.created_at }}</p>-->
+              </div>
+            </div>
+            <div class="card-footer text-body-secondary p-0">
 
-  {{ datas }}
+              <small>{{ datas?.created_at }}</small>
+            </div>
+          </div>
+        </div>
+      </div>
+    </template>
+  </CardsView>
   <pre>
 
     <!-- {{ $store.state }} -->
   </pre>
-
 </template>
 <script>
-import { mapState } from 'vuex';
+import { mapState } from 'vuex'
+import CardsView from '@/components/Cards/CardsView.vue'
+import PlaceHolderLoadingView from '@/components/PlaceHolderLoading/PlaceHolderLoadingView.vue'
 
 export default {
   name: 'AcoesView',
+  components: { PlaceHolderLoadingView, CardsView },
   data() {
-    return {
-
-    }
+    return {}
   },
-  methods: {
-
+  methods: {},
+  computed: {
+    ...mapState('user', ['datas']),
   },
-  computed:{
-    ...mapState('user',['datas'])
-  }
 }
 </script>
 
-<style scoped>
-
-</style>
+<style scoped></style>
