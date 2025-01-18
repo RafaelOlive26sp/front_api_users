d1574725066b2c2906348522c38b84bea892e4a3
Rafael Delgado <oliveirarafael22@outlook.com.br>
2025-01-18 12:39:25 -0300

    raw url.
    adicionado a url para desenvolvimento local

--- src/services/api.js
+++ src/services/api.js
@@ -3,7 +3,8 @@ import Cookies from "js-cookie";
 
 
 const api = axios.create({
-  baseURL: "https://api-users-rafael.up.railway.app/api/v1/",
+  // baseURL: "https://api-users-rafael.up.railway.app/api/v1/",
+  baseURL: "http://127.0.0.1:8000/api/v1/",
   headers: {
     "Content-Type": "application/json",
     Accept: "application/json",
