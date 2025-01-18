e8af4ef55f9477a525b3e0ce1ca0da5d69f7d4aa
Rafael <oliveirarafael22@outlook.com.br>
2025-01-18 02:45:05 -0300

    feat: update API base URL to production endpoint

--- src/services/api.js
+++ src/services/api.js
@@ -1,9 +1,12 @@
 import axios from "axios";
 import Cookies from "js-cookie";
 
+// baseURL: "https://api-users-rafael.up.railway.app/api/v1/",
+// baseURL: "https://api-users-rafael.up.railway.app/api/v1/",
 
 const api = axios.create({
-  baseURL: "http://127.0.0.1:8000/api/v1/",
+  // baseURL: "http://127.0.0.1:8000/api/v1/",
+  baseURL: "https://api-users-rafael.up.railway.app/api/v1/",
   headers: {
     "Content-Type": "application/json",
     Accept: "application/json",
