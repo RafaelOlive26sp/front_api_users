06eb7b1281466d0860c24dd12214d635af5f3f6a
Rafael <oliveirarafael22@outlook.com.br>
2025-01-19 20:52:37 -0300

    feat: switch API base URL to local development endpoint

--- src/services/api.js
+++ src/services/api.js
@@ -5,8 +5,8 @@ import Cookies from "js-cookie";
 // baseURL: "https://api-users-rafael.up.railway.app/api/v1/",
 
 const api = axios.create({
-  // baseURL: "http://127.0.0.1:8000/api/v1/",
-  baseURL: "https://api-users-rafael.up.railway.app/api/v1/",
+  baseURL: "http://127.0.0.1:8000/api/v1/",
+  // baseURL: "https://api-users-rafael.up.railway.app/api/v1/",
   headers: {
     "Content-Type": "application/json",
     Accept: "application/json",
