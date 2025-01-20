08b543bf3616cb59bb32a1592a20db6193e9bf68
Rafael <oliveirarafael22@outlook.com.br>
2025-01-19 22:11:45 -0300

    feat: display authenticated user's name in the navbar

--- src/components/menuNavBars/navBarTopView.vue
+++ src/components/menuNavBars/navBarTopView.vue
@@ -1,7 +1,7 @@
 
 <template>
   <header class="navbar sticky-top bg-dark flex-md-nowrap p-0 shadow" data-bs-theme="dark" >
-    <a class="navbar-brand col-md-3 col-lg-2 me-0 px-3 fs-6 text-white"  >DashBoard - </a>
+    <a class="navbar-brand col-md-3 col-lg-2 me-0 px-3 fs-6 text-white"  >DashBoard - Usuario: {{ $store.state.auth.user.name }}</a>
 
 
     <ul class="navbar-nav flex-row d-md-none">
