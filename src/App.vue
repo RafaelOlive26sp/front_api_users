bde579ee58f3e1018f2c55e0760e65bb1e98109a
Rafael <oliveirarafael22@outlook.com.br>
2025-01-23 20:09:55 -0300

    refactor: clean up unused whitespace and commented code in App.vue and LoginView.vue

--- src/App.vue
+++ src/App.vue
@@ -3,14 +3,9 @@ import { RouterView } from 'vue-router'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import 'bootstrap'
 import 'bootstrap-icons/font/bootstrap-icons.css'
-
-
-
 </script>
 
 <template>
-
-
   <RouterView />
 </template>
 
