8e2c0e6f648e2468dcd094948a4357735e875b78
Rafael <oliveirarafael22@outlook.com.br>
2025-01-31 23:25:02 -0300

    refactor: enhance ModalView layout with centered dialog and responsive design

--- src/components/ModalView.vue
+++ src/components/ModalView.vue
@@ -1,7 +1,7 @@
 
 <template>
   <div class="modal fade"  :id=idModal tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-    <div class="modal-dialog" >
+    <div class="modal-dialog modal-dialog-centered" >
       <div class="modal-content text-dark">
         <slot name="content"></slot>
 
@@ -25,5 +25,22 @@ export default {
 </script>
 
 <style scoped>
+.modal-dialog {
+  max-width: 90%; /* 90% da largura em telas pequenas */
+  margin: 1rem auto;
+}
+
+@media (min-width: 768px) {
+  .modal-dialog {
+    max-width: 600px; /* Define um limite para telas médias e grandes */
+  }
+}
+
+.modal-content {
+  overflow-y: auto; /* Adiciona rolagem vertical se o conteúdo for maior que a altura da tela */
+  max-height: 90vh; /* Limita a altura máxima do modal a 90% da altura da viewport */
+}
+
+
 
 </style>
